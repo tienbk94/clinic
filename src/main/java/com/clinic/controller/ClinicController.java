8d9f1c2a51b3ac9f815a1774c91865560fa15655
iPandaxXx <truongtuanpham129@gmail.com>
2021-12-13 20:21:57 +0700

    xoa thu vien khong su dung

--- src/main/java/com/clinic/controller/ClinicController.java
+++ src/main/java/com/clinic/controller/ClinicController.java
@@ -1,13 +1,10 @@
 package com.clinic.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
